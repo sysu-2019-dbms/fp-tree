dd1682ca2b4f2ce17b15312cc868e7c14c09a593
ctlchild <ctlchild@163.com>
2019-05-03 21:20:06 -0700

    p_allocator

--- src/p_allocator.cpp
+++ src/p_allocator.cpp
@@ -36,10 +36,6 @@ PAllocator::PAllocator()
 	int is_pmem;
     if (allocatorCatalog.is_open() && freeListFile.is_open())
     {
-        // exist
-        // allocatorCatalog.read((char *)&maxFileId, sizeof(uint64_t));
-        // allocatorCatalog.read((char *)&freeNum, sizeof(uint64_t));
-        // allocatorCatalog.read((char *)&startLeaf, sizeof(PPointer));
         
         pmemaddr=(char*)pmem_map_file(allocatorCatalogPath.c_str(), 
                             sizeof(u_int64_t)*2+sizeof(PPointer),
@@ -62,8 +58,6 @@ PAllocator::PAllocator()
         freeList.clear();
         for (int i = 0; i < freeNum; i++)
         {
-            // PPointer p;
-            // freeListFile.read((char *)&p, sizeof(PPointer));
             PPointer p=*((PPointer*)pmemaddr);
             pmemaddr+=sizeof(PPointer);
             freeList.push_back(p);
@@ -71,11 +65,6 @@ PAllocator::PAllocator()
     }
     else
     {
-        // ofstream ofcatalog;
-        // ofcatalog.open(allocatorCatalogPath, ios::out | ios::binary);
-        // ofstream offreelist;
-        // offreelist.open(freeListPath, ios::out | ios::binary);
-        //not exist, create catalog and free_list file, then open.
         pmem_map_file(allocatorCatalogPath.c_str(), 
                             sizeof(u_int64_t)*2+sizeof(PPointer),
                             PMEM_FILE_CREATE,
@@ -88,6 +77,9 @@ PAllocator::PAllocator()
 				            0666,
                             &mapped_len,
                             &is_pmem);
+        maxFileId=1;
+        freeNum=0;
+        freeList.clear();
     }
     this->initFilePmemAddr();
 }
@@ -101,20 +93,34 @@ PAllocator::~PAllocator()
     char *pmemaddr; 
     for (int i=1;i<maxFileId;i++){
         pmemaddr=(char *)pmem_map_file((DATA_DIR + std::to_string(i)).c_str(),
-                                           8 + LEAF_GROUP_AMOUNT +
-                                               LEAF_GROUP_AMOUNT * (sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE,
+                                           LEAF_GROUP_HEAD +
+                                               LEAF_GROUP_AMOUNT * (calLeafSize()),
                                            PMEM_FILE_CREATE,
                                            0666,
                                            &mapped_len,
                                            &is_pmem);   
+        if (is_pmem)
+		    pmem_persist(pmemaddr, mapped_len);
+	    else
+		    pmem_msync(pmemaddr, mapped_len);
         pmem_unmap(pmemaddr,mapped_len);
     }
+
     pmemaddr=(char *)pmem_map_file(allocatorCatalogPath.c_str(), 
                             sizeof(u_int64_t)*2+sizeof(PPointer),
                             PMEM_FILE_CREATE,
 				            0666,
                             &mapped_len,
                             &is_pmem);
+    *((uint64_t*)pmemaddr)=maxFileId;
+    pmemaddr+=8;
+    *((uint64_t*)pmemaddr)=freeNum;
+    pmemaddr+=8;
+    *((PPointer*)pmemaddr)=startLeaf;
+    if (is_pmem)
+		pmem_persist(pmemaddr, mapped_len);
+	else
+		pmem_msync(pmemaddr, mapped_len);
     pmem_unmap(pmemaddr,mapped_len);
 
     pmemaddr=(char *)pmem_map_file(freeListPath.c_str(), 
@@ -123,7 +129,23 @@ PAllocator::~PAllocator()
 				            0666,
                             &mapped_len,
                             &is_pmem);
+    for (int i=0;i<freeNum;i++){
+        PPointer p=freeList[i];
+        *((uint64_t*)pmemaddr)=p.fileId;
+        pmemaddr+=8;
+        *((uint64_t*)pmemaddr)=p.offset;
+        pmemaddr+=8;
+    }
+    if (is_pmem)
+		pmem_persist(pmemaddr, mapped_len);
+	else
+		pmem_msync(pmemaddr, mapped_len);
     pmem_unmap(pmemaddr,mapped_len);
+    fId2PmAddr.clear();
+    freeList.clear();
+    maxFileId=1;
+    freeNum=0;
+    PAllocator::pAllocator = NULL;
 }
 
 // memory map all leaves to pmem address, storing them in the fId2PmAddr
@@ -135,8 +157,8 @@ void PAllocator::initFilePmemAddr()
     char *pmemaddr;
     for (int i=1;i<maxFileId;i++){
         pmemaddr=(char *)pmem_map_file((DATA_DIR + std::to_string(maxFileId)).c_str(),
-                                           8 + LEAF_GROUP_AMOUNT +
-                                               LEAF_GROUP_AMOUNT * (sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE,
+                                           LEAF_GROUP_HEAD +
+                                               LEAF_GROUP_AMOUNT * (calLeafSize()),
                                            PMEM_FILE_CREATE,
                                            0666,
                                            &mapped_len,
@@ -162,14 +184,33 @@ bool PAllocator::getLeaf(PPointer &p, char *&pmem_addr)
     {
         if (!newLeafGroup()) return false; 
     }
-    
     p = freeList.back();
     freeList.pop_back();
     freeNum--;
-    pmem_addr = getLeafPmemAddr(p);
-    if (pmem_addr != NULL) return true;
     
-    return false;
+    size_t mapped_len;
+    int is_pmem;
+    char *pmemaddr; 
+    pmemaddr=(char*)pmem_map_file((DATA_DIR + std::to_string(p.fileId)).c_str(), 
+                            LEAF_GROUP_HEAD + LEAF_GROUP_AMOUNT * (calLeafSize()),
+                            PMEM_FILE_CREATE,
+				            0666,
+                            &mapped_len,
+                            &is_pmem);
+    if (pmemaddr==NULL) return false;
+    uint64_t last=(*((uint64_t*)pmemaddr));
+    *((uint64_t*)pmemaddr)=last+1;
+    pmemaddr+=sizeof(uint64_t);
+    int pos=(p.offset-8-LEAF_GROUP_AMOUNT)/ 
+            ((calLeafSize()));
+    *(pmemaddr+pos)=1;
+    if (is_pmem)
+		pmem_persist(pmemaddr, mapped_len);
+	else
+		pmem_msync(pmemaddr, mapped_len);
+
+
+    return true;
 }
 
 bool PAllocator::ifLeafUsed(PPointer p)
@@ -177,7 +218,7 @@ bool PAllocator::ifLeafUsed(PPointer p)
     if (ifLeafExist(p))
     {
         int pos=(p.offset-8-LEAF_GROUP_AMOUNT)/ 
-            ((sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE);
+            ((calLeafSize()));
         if (*(fId2PmAddr[p.fileId]+8+pos)==1) return true;
     }
     return false;
@@ -188,7 +229,7 @@ bool PAllocator::ifLeafFree(PPointer p)
     if (ifLeafExist(p))
     {
         int pos=(p.offset-8-LEAF_GROUP_AMOUNT)/ 
-            ((sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE);
+            ((calLeafSize()));
         if (*(fId2PmAddr[p.fileId]+8+pos)==0) return true;
     }
     return false;
@@ -198,7 +239,7 @@ bool PAllocator::ifLeafFree(PPointer p)
 bool PAllocator::ifLeafExist(PPointer p)
 {
     if (fId2PmAddr.count(p.fileId)&&p.offset>=8+LEAF_GROUP_AMOUNT&&
-    p.offset<8 + LEAF_GROUP_AMOUNT +  LEAF_GROUP_AMOUNT* (sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE)
+    p.offset<LEAF_GROUP_HEAD +  LEAF_GROUP_AMOUNT* (calLeafSize()))
         return true;
     else
         return false;
@@ -209,6 +250,9 @@ bool PAllocator::freeLeaf(PPointer p)
 {
     if (ifLeafUsed(p))
     {
+        int pos=(p.offset-8-LEAF_GROUP_AMOUNT)/ 
+            ((calLeafSize()));
+        *(fId2PmAddr[p.fileId]+8+pos)=0;
         freeList.push_back(p);
         freeNum++;
         return true;
@@ -247,6 +291,7 @@ bool PAllocator::persistCatalog()
 	else
 		pmem_msync(pmemaddr, mapped_len);
     return true;
+    //return false;
 }
 
 /*
@@ -260,18 +305,20 @@ bool PAllocator::newLeafGroup()
     size_t mapped_len;
     int is_pmem;
     char *pmemaddr = (char *)pmem_map_file((DATA_DIR + std::to_string(maxFileId)).c_str(),
-                                           8 + LEAF_GROUP_AMOUNT +
-                                               LEAF_GROUP_AMOUNT * (sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE,
+                                           LEAF_GROUP_HEAD +
+                                               LEAF_GROUP_AMOUNT * (calLeafSize()),
                                            PMEM_FILE_CREATE,
                                            0666,
                                            &mapped_len,
                                            &is_pmem);
+    if (pmemaddr==NULL) return false;
     for (int i = 0; i < LEAF_GROUP_AMOUNT; i++)
     {
-        freeList.push_back((PPointer){maxFileId, 8 + LEAF_GROUP_AMOUNT + i * (sizeof(MAX_KEY) + sizeof(MAX_VALUE)) * LEAF_DEGREE});
+        freeList.push_back((PPointer){maxFileId, LEAF_GROUP_HEAD + i * (calLeafSize())});
+        PPointer p=freeList.back();
     }
     fId2PmAddr[maxFileId] = pmemaddr;
     freeNum += LEAF_GROUP_AMOUNT;
     maxFileId++;
-    return false;
+    return true;
 }
